7f69a9894bb1e86f3dfb4534bb31f8409541a44e
game00585-star <game00585@gmail.com>
2026-09-23 14:22:57 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,6 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import App from './App';import './index.css';import './pages/workflowDetails.css';ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+import React from 'react';import ReactDOM from 'react-dom/client';import App from './App';import './index.css';import './pages/workflowDetails.css';
+
+const savedTheme=localStorage.getItem('csp_dark_mode');
+document.documentElement.classList.toggle('dark-theme',savedTheme!=='false');
+
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
